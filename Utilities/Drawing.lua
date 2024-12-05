13f26d98847436ccdb7df953cb45555894806acc
bytepseudo <165760120+bytepseudo@users.noreply.github.com>
2024-12-05 04:11:44 -0500

    Update Drawing.lua

--- Utilities/Drawing.lua
+++ Utilities/Drawing.lua
@@ -1,55 +1,5 @@
-print('gg')
+local module = {}
 
-local Players = game:GetService('Players')
-local RunService = game:GetService('RunService')
+module.test = 21312313
 
-local Camera = Workspace.CurrentCamera
-
-local Drawings = {
-    Objects = {}
-}
-
-function Drawings:AddDrawing(Type, Properties)
-    local DrawingObject = Drawing.new(Type)
-
-    for Property, Value in pairs(Properties) do
-        DrawingObject[Property] = Value
-    end
-
-    return DrawingObject
-end
-
-function Drawings:AddObject(Instance, Options)
-    if self.Objects[Instance] then return end
-
-    local Object = self.Objects[Instance] = {}
-
-    Object.Connection = nil
-
-    local Text = self:AddDrawing('Text', {
-        Text = Options.Text,
-        Visible = Options.Visible,
-        Center = Options.Center,
-        Outline = Options.Outline,
-        Color = Options.Color,
-        OutlineColor = Options.OutlineColor
-    })
-
-    Object.Connection = RunService.RenderStepped:Connect(function()
-        if not Instance then
-            Object.Connection:Disconnect()
-            Object = nil
-        end
-
-        local ScreenPosition, OnScreen = Camera:WorldToScreenPoint(Instance.Position)
-
-        if OnScreen then
-            Text.Visible = true
-            Text.Position = Vector2.new(ScreenPosition.X, ScreenPosition.Y)
-        else
-            Text.Visible = false
-        end
-    end)
-end
-
-return Drawings
+return module
