ac4adf2744c557dc402d42807ed81da3673cdf75
bytepseudo <165760120+bytepseudo@users.noreply.github.com>
2024-12-08 20:00:24 -0500

    Update Drawing.lua

--- Utilities/Drawing.lua
+++ Utilities/Drawing.lua
@@ -17,4 +17,37 @@ Drawings.AddDrawing = function(Type, Properties)
     return DrawingObject
 end
 
+Drawings.AddObject = function(Instance, Options)
+    if self.Objects[Instance] then return end
+
+    local Object = self.Objects[Instance] = {}
+
+    Object.Connection = nil
+
+    local Text = self:AddDrawing('Text', {
+        Text = Options.Text,
+        Visible = Options.Visible,
+        Center = Options.Center,
+        Outline = Options.Outline,
+        Color = Options.Color,
+        OutlineColor = Options.OutlineColor
+    })
+
+    Object.Connection = RunService.RenderStepped:Connect(function()
+        if not Instance then
+            Object.Connection:Disconnect()
+            Object = nil
+        end
+
+        local ScreenPosition, OnScreen = Camera:WorldToScreenPoint(Instance.Position)
+
+        if OnScreen then
+            Text.Visible = true
+            Text.Position = Vector2.new(ScreenPosition.X, ScreenPosition.Y)
+        else
+            Text.Visible = false
+        end
+    end)
+end
+
 return Drawings
