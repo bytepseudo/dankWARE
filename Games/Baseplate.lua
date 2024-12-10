e3a3ee6498d97be1df09210f8891307b1fdc8415
byte <165760120+bytepseudo@users.noreply.github.com>
2024-12-10 00:30:17 -0500

    Update Baseplate.lua

--- Games/Baseplate.lua
+++ Games/Baseplate.lua
@@ -1,1 +1,1 @@
-print(dankWARE)
+dankWARE.Utilities.Interface:Toast({Title = `Hello from the otherside! {math.floor((tick() - dankWARE.StartTime) * 10) / 10}`, Duration = 1.5, Color = Color3.new(0.0902, 0.65098, 0.92941, 0)})
