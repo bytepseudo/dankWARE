17c04df6c871c6688c4ab62bcc9bb12b0a708f58
byte <165760120+bytepseudo@users.noreply.github.com>
2024-12-10 00:15:59 -0500

    Update Baseplate.lua

--- Games/Baseplate.lua
+++ Games/Baseplate.lua
@@ -1,1 +1,1 @@
-dankWARE.Utilities.Interface:Toast({Title = 'Hello from the otherside!', Duration = 1.5, Color = Color3.new(0.0902, 0.65098, 0.92941, 0)})
+getgenv().dankWARE.Utilities.Interface:Toast({Title = 'Hello from the otherside!', Duration = 1.5, Color = Color3.new(0.0902, 0.65098, 0.92941, 0)})
