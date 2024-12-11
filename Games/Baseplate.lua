91932bec435cdc8659b6f5f2f6465366b968b117
byte <165760120+bytepseudo@users.noreply.github.com>
2024-12-11 02:40:01 -0500

    Update Baseplate.lua

--- Games/Baseplate.lua
+++ Games/Baseplate.lua
@@ -1,11 +1,13 @@
-local CombatText = dankWARE.Utilities.Drawing:AddDrawing('Text', {
+local PositionText = dankWARE.Utilities.Drawing:AddDrawing('Text', {
     Visible = true,
     Position = Vector2.new(960, 960),
     Center = true,
     Outline = true,
     Color = Color3.new(1, 1, 1),
     OutlineColor = Color3.new(0, 0, 0),
-    Text = 'Enabled: false, Target: None'
+    Text = 'Position: 0, 0, 0'
 })
 
-task.wait(5)
+game.RunService.RenderStepped:Connect(function()
+    PositionText.Text = `Position: {tostring(game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame.Position)}`
+end)
